a85c1a7303c5e9137eff3a1b41ee3d51f8b4457b
Armatik <s.fomchenkov@yandex.ru>
2022-11-02 22:33:25 +0300

    Day 7.6(Home)

--- Lab7/Kniga_Day_5_7.cpp
+++ Lab7/Kniga_Day_5_7.cpp
@@ -1,39 +1,7 @@
-//#include <iostream>
-//
-//
-//double degree(double number, unsigned numberDegree);
-//
-//
-//int main() {
-//    double number, result;
-//    unsigned numberDegree;
-//
-//    std::cout << "Enter the number and number's degree seperated by a space respectively: "; std::cin >> number >> numberDegree;
-//
-//    result = degree(number, numberDegree);
-//    std::cout << result << std::endl;
-//
-//    return 0;
-//}
-//
-//
-//double degree(double number, unsigned numberDegree) {
-//    static double constant = number;
-//    number *= constant;
-//    numberDegree--;
-//
-//    if (numberDegree)
-//        return degree(number, numberDegree);
-//
-//    return number / constant;
-//}
-#include <cmath>
 #include <iostream>
 
 
 double degree(double number, unsigned numberDegree);
-double degree(double number, int numberDegree);
-double degree(double number, double numberDegree);
 
 
 int main() {
@@ -58,21 +26,4 @@ double degree(double number, unsigned numberDegree) {
         return degree(number, numberDegree);
 
     return number / constant;
-}
-
-
-double degree(double number, int numberDegree) {
-    static double constant = number;
-    number *= constant;
-    numberDegree++;
-
-    if (numberDegree)
-        return degree(number, numberDegree);
-
-    return 1 / (number / constant);
-}
-
-
-double degree(double number, double numberDegree) {
-    sqrt()
 }
