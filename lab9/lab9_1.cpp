0ed644493fa9ea3fee38429be1b8936d97a00147
Armatik <s.fomchenkov@yandex.ru>
2022-11-16 19:50:43 +0300

    Day 9.3(Home)

--- lab9/lab9_1.cpp
+++ lab9/lab9_1.cpp
@@ -1,49 +1,39 @@
-//#include <iostream>
-//#include <string>
-//#include <random>
-////#include "Lab2/16.cpp"
-//
-//
-//std::random_device rd;
-//std::mt19937 gen(rd());
-//std::uniform_int_distribution<> dist(0, 255);
-//
-//
-//
-//char* generator(unsigned &size);
-//
-//
-//int main() {
-//    unsigned secondLaySize = dist(gen);
-//    auto **txt = new char *[secondLaySize];
-//
-//    for (unsigned i = 0; i < secondLaySize; i++) {
-//        unsigned firstLaySize = dist(gen);
-//
-//        txt[i] = new char[firstLaySize];
-//
-//        char *array = generator(firstLaySize);
-//
-//        txt[i] = array;
-//    }
-//
-//    for (unsigned i = 0; i < secondLaySize; i++)
-//        std::cout << i << " = " << txt[i] << std::endl;
-//
-//    for (unsigned i = 0; i < secondLaySize; i++)
-//        delete[] txt[i];
-//
-//    delete[] txt;
-//
-//    return 0;
-//}
-//
-//
-//char* generator(unsigned &size) {
-//    auto array = new char[size];
-//
-//    for (unsigned i = 0; i < size; i++)
-//        array[i] = (char)dist(gen);
-//
-//    return array;
-//}
+#include <iostream>
+#include <string>
+#include <random>
+//#include "Lab2/16.cpp"
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dist(0, 127);
+
+
+
+char* generator(unsigned &size) {
+    auto array = new char[size];
+
+    for (unsigned i = 0; i < size; i++)
+        array[i] = (char)dist(gen);
+
+    return array;
+}
+
+
+int main(){
+    unsigned secondLaySize = dist(gen);
+    auto **txt = new char*[secondLaySize];
+
+    for (unsigned i = 0; i < secondLaySize; i++) {
+        unsigned firstLaySize = dist(gen);
+
+        txt[i] = new char[firstLaySize];
+
+        char *array = generator(firstLaySize);
+
+        txt[i] = array;
+    }
+
+    for (unsigned i = 0; i < secondLaySize; i++)
+        std::cout << i << " = " << txt[i] << std::endl;
+
+    return 0;
+}
